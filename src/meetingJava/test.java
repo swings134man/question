72aad17e6fc68cfdd8063936bc8d7b3b18561862
Seok Jun Kang <swings134@gmail.com>
2022-03-17 15:02:30 +0900

    코딩테스트 업로드

--- src/meetingJava/test.java
+++ src/meetingJava/test.java
@@ -1,10 +1,23 @@
 package meetingJava;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class test {
 
 	public String test() {
 		String a = "a";
 		return a;
 	}//생성자.
+	
+	public List<Integer> test1() {
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		
+		return list;
+	}//test
 	
 }
