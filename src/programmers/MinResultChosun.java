a9987ee662e4b34a205a7b835ed4a7d96b470303
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2022-10-30 18:23:16 +0900

    coding Test

---
+++ src/programmers/MinResultChosun.java
@@ -0,0 +1,63 @@
+package programmers;
+
+public class MinResultChosun {
+
+	public static void main(String[] args) {
+
+//		class Solution {
+//		    public int solution(int no, int[] works) {
+//		        int result = 0;
+//
+//		        // 큰수 먼저 정렬
+//		        reverse(works);
+//
+//		        // 배상 비용 최소화 ? 
+//		        // 가장 큰 수 해결 ? -> works 에서 n 값만큼 {최대값} -1 -> 
+//
+//		        int max = maxValue(works);
+//		        System.out.println(max); // 값 
+//
+//		        // while(no > 0) {
+//		        //     int max = maxValue(works);
+//		        //      // 초기값 -> 시간복잡도 O(n^2)
+//		            
+//		        //     works[max] -= 1;
+//		        //     no -= 1;
+//		        // }
+//
+//		        // for(int i : works) {
+//		        //     result += (i*2);
+//		        // }
+//
+//		        return result;
+//		    }
+//
+//		    // reverse 정렬 - int 타입정렬을 위함 (Integer X)
+//		    private static void reverse(int[] arr) {
+//		        Arrays.sort(arr); // 오름차순 정렬
+//
+//		        for(int i=0; i < arr.length/2; i++ ) {
+//		            int basic = arr[i]; // 초기값.
+//		 
+//		            arr[i] = arr[arr.length -i -1];
+//		            arr[arr.length -i -1] = basic;
+//		        }
+//		    }//method
+//
+//		    //최대값
+//		    private static int maxValue(int[] arr) {
+//		        int basic = arr[0];
+//		        int maxIndex = 0;
+//
+//		      for(int i = 0; i < arr.length; i++) {
+//		          if(arr[i] > basic) {
+//		            maxIndex = i;
+//		          }
+//		        }
+//		        return maxIndex;
+//		    }
+//
+//		}
+	}
+
+}
