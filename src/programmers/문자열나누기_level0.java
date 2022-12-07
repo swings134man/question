3bf0929082dc1dd262e7238283f945c2e6fa242d
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2022-12-07 17:46:46 +0900

    문자열나누기 fix

--- src/programmers/문자열나누기_level0.java
+++ src/programmers/문자열나누기_level0.java
@@ -22,16 +22,19 @@ public class 문자열나누기_level0 {
 		
 		
 		for(int i=0; i<s.length(); i++) {
+			// 1-1
 			if(one == ' ') {
 				one = s.charAt(i);
 			}
 			
+			// 1-2
 			if(one == s.charAt(i)) {
 				same++;
 			}else {
 				diff++;
 			}
 			
+			// 1-3
 			if(same == diff) {
 				result++;
 				one = ' ';
@@ -40,10 +43,12 @@ public class 문자열나누기_level0 {
 			}
 		}
 		
+		// 2-1
 		if(same != 0) {
 			result++;
 		}
 		
+		// 3-1
 		//return 
 		if(result == 0) {
 			result = 1;
@@ -53,12 +58,13 @@ public class 문자열나누기_level0 {
 		return result;
 	}
 	
-
+	
+	
 	public static void main(String[] args) {
 		String a = "banana";
 		int result = solution(a);
 		
-		System.out.println(result);
+		System.out.println(solution(a));
 		
 	} //main
 }//class
