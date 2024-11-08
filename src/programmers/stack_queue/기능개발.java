5f5ccaa319885e8d375bb4d4ad0b9af3888d95ac
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2024-11-08 17:46:08 +0900

    feature: queue(프로그래머스) 다시,,

--- src/programmers/stack_queue/기능개발.java
+++ src/programmers/stack_queue/기능개발.java
@@ -13,33 +13,33 @@ public class 기능개발 {
     }
 
     public int[] solution(int[] progresses, int[] speeds) {
-        Queue<Integer> queue = new LinkedList();
+        Queue<Integer> queue = new LinkedList<>(); // 배포에 필요한 일수
         List<Integer> list = new ArrayList<>();
-
+        
+        // Queue 에 몇일 걸리는지 적재
         for (int i = 0; i < progresses.length; i++) {
-            // 당일
-             if(((100 - progresses[i]) & speeds[i]) == 0){
-                 queue.offer((100 - progresses[i]) / speeds[i]);
-             }else {
-                 queue.offer((100 - progresses[i]) / speeds[i] + 1);
-             }
+            // 100 % 완료라면?
+            if((100 - progresses[i]) / speeds[i] == 0) {
+                queue.offer((100 - progresses[i]) / speeds[i]);
+            }else {
+                queue.offer((100 - progresses[i]) / speeds[i] + 1); // 0이 아니면 1일이 더 걸림
+            }
         }
-
-        // 첫 배포
+        
         int now = queue.poll();
         int count = 1;
-
-        while (!queue.isEmpty()) {
-            if(now >= queue.peek()){
-                count ++;
-                queue.poll();
+        // 비교 시작
+        while (!queue.isEmpty()){
+            if(now >= queue.peek()) { // 다음 요소가, 첫번쨰 배포일과 같거나 작다면? 같이배포가능
+                count++;
             }else {
-                list.add(count);
-                count = 1;
-                now = queue.poll();
+                list.add(count); // 몇일
+                count = 1; //초기화
+                now = queue.poll(); // 다음 배포일자?
             }
         }
         list.add(count);
+
         return list.stream().mapToInt(i -> i).toArray();
     }
 }
