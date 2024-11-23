50644804be237a71b82ab3db6f40e9b848624d1d
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2024-11-23 23:46:42 +0900

    feature: 백준(심화: 체스)

--- src/testCode/MemoClass.java
+++ src/testCode/MemoClass.java
@@ -2,11 +2,6 @@ package testCode;
 
 public class MemoClass {
     public static void main(String[] args) {
-        char word = 'a';
-        char word2 = 'b';
-        System.out.println((int) word);
-        System.out.println((int) word + 1);
 
-        System.out.println((int) word2);
     }
 }
